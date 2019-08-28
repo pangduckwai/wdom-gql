305be08072ae7053d6dc2fa3109123d940dd7f19
pangduckwai <paul.lai@sea9.org>
2019-08-28 13:46:35 +0800

    Update npm modules

--- client/src/components/map-drag.js
+++ client/src/components/map-drag.js
@@ -4,16 +4,6 @@ import './app.css';
 export default function DragIcon(props) {
 	let clazz = (props.dragging) ? 'drag show' : 'drag hide';
 
-	// const handleHover = (e) => {
-	// 	console.log("No-op");
-	// };
-
-	// TODO: use path to draw a soldier...  transform={`translate(${props.xpos}, ${props.ypos})`}
-	// return (
-	// 	<polyline id="drag-icon"
-	// 		className={clazz}
-	// 		points="-20,-17 0,17 20,-17 -20,-17" />
-	// );
 	return (
 		<>
 			<line id="drag-icon-0" className={clazz}
