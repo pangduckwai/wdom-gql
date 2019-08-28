38837269b5c4a2fad3c91816c51d0fe9aabd99d0
pangduckwai <paul.lai@sea9.org>
2019-08-29 01:35:17 +0800

    Add cards display (WIP)

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function Cards(props) {
 	const cards = (props.cards && (props.cards.length > 0)) ? props.cards : [
-		{ name: "Congo", type: "Artillery" }, { name: "Alberta", type: "Infantry" }
+		{ name: "Congo", type: "Artillery" }, { name: "Western-United-States", type: "Infantry" }, { name: "Alberta", type: "Infantry" },
+		{ name: "Congo", type: "Artillery" }, { name: "Congo", type: "Artillery" }
 	];
 
 	return (
@@ -10,7 +11,12 @@ export default function Cards(props) {
 			{props.playerToken &&
 				<ul>
 					{cards.map(c =>
-						(<li>{c.name} {c.type}</li>)
+						(<li>
+							<div className="card">
+								<div>{c.name}</div>
+								<div>{c.type}</div>
+							</div>
+						</li>)
 					)}
 				</ul>
 			}
