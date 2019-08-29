037a3521534ebd93b9ee89a41538a31d03804b55
pangduckwai <paul.lai@sea9.org>
2019-08-29 13:33:20 +0800

    Add selecting cards

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -1,25 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Cards(props) {
+	const [compKey, setCompKey] = useState(0);
+	const [values, setValues] = useState([]);
+	const [scroll, setScroll] = useState(0);
+
+	useEffect(() => {
+		const list = document.getElementById("card-list");
+		if (list) {
+			list.scrollLeft = scroll;
+		}
+	});
+
+	//TODO TEMP!!!!!!!!!!!!!!!!!
 	const cards = (props.cards && (props.cards.length > 0)) ? props.cards : [
 		{ name: "Congo", type: "Artillery" }, { name: "Western-United-States", type: "Infantry" }, { name: "Alberta", type: "Infantry" },
-		{ name: "Congo", type: "Artillery" }, { name: "Congo", type: "Artillery" }
+		{ name: "China", type: "Artillery" }, { name: "India", type: "Infantry" }
 	];
 
+	const handleSelect = (e) => {
+		let array = values.map(c => c);
+		let selected = e.target.value;
+
+		const index = array.indexOf(selected);
+		if (index >= 0) {
+			array.splice(index, 1);
+		} else {
+			array.push(selected);
+			if (array.length > 3) array.shift();
+		}
+
+		setValues(array);
+
+		const list = document.getElementById("card-list");
+		setScroll(list.scrollLeft);
+		setCompKey(compKey + 1);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("Redeem...", JSON.stringify(values));
+	};
+
 	return (
-		<div id="cards">
+		<>
 			{props.playerToken &&
-				<ul>
-					{cards.map(c =>
-						(<li>
-							<div className="card">
-								<div>{c.name}</div>
-								<div>{c.type}</div>
-							</div>
-						</li>)
-					)}
-				</ul>
+				<form key={compKey}
+					id="cards"
+					onSubmit={handleSubmit}>
+					<ul id="card-list" onChange={handleSelect}>
+						{cards.map(c =>
+							(<li key={c.name}>
+								<div className="card">
+									<div className="card-name">{c.name}</div>
+									<div className="card-type">{c.type}</div>
+									<input
+										type="checkbox"
+										value={c.name}
+										defaultChecked={values.includes(c.name)} />
+								</div>
+							</li>)
+						)}
+					</ul>
+					<input type="submit" value="&crarr;" />
+				</form>
 			}
-		</div>
+		</>
 	);
 }
